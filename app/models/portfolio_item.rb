abd85d99ca86e8c04ea73b8b93258af71f41fb9b
Ben Arnold <ben@seawolfsanctuary.com>
2013-05-22 20:54:07 +0100

    Add: some portfolio style improvements

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -1,14 +1,15 @@
 class PortfolioItem  # < ActiveRecord::Base
   def id
-    return 4
+    return rand(5) + 1
   end
 
   def title
-    return "Title!"
+    titles = [ "Skeleton", "Eagle", "Shrunken Head", "Chicken", "Sausage Monkey", "Banana Baboon" ]
+    return titles[rand(5)]
   end
 
   def description
-    return "Description!"
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec mi purus, non convallis libero. Praesent sit amet massa quam. Ut id leo enim, et posuere ipsum. Maecenas semper orci ac augue pretium at aliquet dolor consectetur. In scelerisque lectus ut sem viverra posuere. Fusce volutpat ultrices consequat. In tempor euismod elit, quis dignissim nibh bibendum quis. Etiam convallis mollis orci, fermentum sodales nisi adipiscing quis."
   end
 
   def image_filename_1
