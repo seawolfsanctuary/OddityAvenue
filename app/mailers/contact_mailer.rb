7de6d69e28a385a188a791bcf2e994577d917bbe
Ben Arnold <ben@seawolfsanctuary.com>
2013-06-08 15:23:22 +0100

    Fix: email settings

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,11 +4,15 @@ class ContactMailer < ActionMailer::Base
   def contact_email message, name, email, subject
     @env = Rails.configuration.action_mailer.smtp_settings[:domain] || 'oddityavenue.com'
     @time = Time.now.strftime("%d/%m/%Y %H:%M")
+    @from_name = name.to_s
+    @from_email = email.to_s
     @message = message.to_s
 
+    to = StaticContent.load("contact", "email")
+
     mail(
-      :from    => "#{name} <#{email}>",
-      :to      => 'webmaster@seawolfsanctuary.com',
+      :from    => "OddityAvenue E-Mail Robot <webmaster@seawolfsanctuary.com>",
+      :to      => to,
       :subject => "OddityAvenue Contact - #{subject}"
     )
   end
