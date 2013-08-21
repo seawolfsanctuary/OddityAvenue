e71d58dd700f4f11cfb45336d727302ff5aefb9d
Ben Arnold <ben@seawolfsanctuary.com>
2013-08-21 19:23:02 +0100

    Fix: only use categries where *enabled* Items exist

--- spec/views/shop/_shop_categories_spec.rb
+++ spec/views/shop/_shop_categories_spec.rb
@@ -15,7 +15,8 @@ describe 'shop/_shop_categories' do
 
   context "with ShopItem categories" do
     before do
-      FactoryGirl.create :shop_item, category_list: [ "cat 1", "cat 2" ], enabled: false
+      FactoryGirl.create :shop_item, category_list: [ "cat 1", "cat 2" ]
+      FactoryGirl.create :shop_item, category_list: [ "cat 2", "cat 3" ], enabled: false
     end
 
     it "should create a new list" do
@@ -24,10 +25,15 @@ describe 'shop/_shop_categories' do
       rendered.should be_include("</ul>")
     end
 
-    it "should create a new list item for each category" do
+    it "should create a new list item for each category where enabled items exist" do
       render
       rendered.should be_include("<li>" + link_to("cat 1", shop_item_category_path(URI::escape("cat 1")))  + "</li>")
       rendered.should be_include("<li>" + link_to("cat 2", shop_item_category_path(URI::escape("cat 2")))  + "</li>")
+    end
+
+   it "should not create a new list item for each category where only disabled items exist" do
+      render
+      rendered.should_not be_include("cat 3")
     end
   end
 end
