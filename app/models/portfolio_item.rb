7e78e30eafc8e1b73eed9ec64cbda60a040fcaea
Ben Arnold <ben@seawolfsanctuary.com>
2013-09-07 15:05:28 +0100

    Add: PortfolioItem#move to create a ShopItem and then delete self

--- app/models/portfolio_item.rb
+++ app/models/portfolio_item.rb
@@ -5,4 +5,26 @@ class PortfolioItem < ActiveRecord::Base
   attr_accessible :title, :description,
     :image_filename_1, :image_filename_2, :image_filename_3,
     :thumbnail_filename, :category_list, :enabled
+
+  def move
+    # -1 couldn't create
+    #  0 couldn't destroy
+    #  1 success
+
+    destination = ShopItem.new
+
+    attributes.each do |key, value|
+      begin
+        destination.send("#{key}=".to_sym, value)
+      rescue NoMethodError ; end
+    end
+
+    destination.category_list = categories.split(ActsAsTaggableOn.delimiter)
+
+    if destination.save
+      return (destroy.present? ? 1 : 0)
+    else
+      return -1
+    end
+  end
 end
